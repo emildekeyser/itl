ad48537ea7c2259662aed99ce20d97fdfea14674
emil <emil.dekeyser@student.ucll.be>
2019-07-18 11:24:36 +0200

    removed junk output data

--- XML2M3U.py
+++ XML2M3U.py
@@ -1,71 +1,68 @@
-import plistlib, sys, os
+import plistlib, sys, os, urllib.parse
+
+EXCLUDED_PLAYLISTS = [
+    '####!####',
+    'Music',
+    'Music Videos',
+    'Rentals',
+    'Films',
+    'Home Videos',
+    'TV Programmes',
+    'Podcasts',
+    'iTunes U',
+    'Books',
+    'Books',
+    'PDFs',
+    'Audiobooks',
+    'Genius',
+    'Play'
+]
+PATH_SEPARATOR = '==='
+
+def load_itl():
+    xmlpath = 'itl.xml'
+    xml = plistlib.load(open(xmlpath, 'rb'))
+    global TRACKS
+    global PLAYLISTS
+    TRACKS = xml['Tracks']
+    PLAYLISTS = {}
+    for p in xml['Playlists']:
+        if p.get('Name') not in EXCLUDED_PLAYLISTS:
+            PLAYLISTS[p.get('Playlist Persistent ID')] = p
 
 def main():
+    load_itl()
+    for pid in PLAYLISTS:
+        pls_path = with_parents(pid)
+        print(pls_path)
+        writepls(pls_path, pid)
 
-    xmlpath = 'itl.xml'
-    print('Reading xml file ...')
-    # xml = plistlib.readPlist(xmlpath)
-    xml = plistlib.load(open(xmlpath, 'rb'))
-    print('Reading tracks ...')
-    tracks = xml['Tracks']
-    print('Reading playlists ... ')
-    playlists = xml['Playlists']
-    print('Start!')
 
-    folders = dict()
-    root = list()
-    for p in playlists:
-        dirname = p.get('Parent Persistent ID')
-        if dirname == None:
-            root.append(p)
-        else:
-            if folders.get(dirname) == None:
-                folders[dirname] = list()
-            folders[dirname].append(p)
-        names = list()
-        for rootitem in root:
-            if folders.get(rootitem['Playlist Persistent ID']) != None:
-                folders[rootitem['Name']] = folders[rootitem['Playlist Persistent ID']]
-                names.append(rootitem['Name'])
-        for f in list(folders):
-            if f not in names:
-                del folders[f]
+def with_parents(pid):
+    p = PLAYLISTS[pid]
+    parent_pid = p.get('Parent Persistent ID')
+    path = PATH_SEPARATOR + p.get('Name')
+    if parent_pid != None:
+        return with_parents(parent_pid) + path
+    return path
 
-        names.extend(
-            {
-                '####!####',
-                'Music',
-                'Music Videos',
-                'Rentals',
-                'Films',
-                'Home Videos',
-                'TV Programmes',
-                'Podcasts',
-                'iTunes U',
-                'Books',
-                'Books',
-                'PDFs',
-                'Audiobooks',
-                'Genius',
-                'Play'
-            })
 
-        root = [p for p in root if p['Name'] not in names]
+    #     root = [p for p in root if p['Name'] not in names]
 
-        rootpath = 'plsv2/'
-        os.makedirs(rootpath, exist_ok=True)
-        for p in root:
-            p['Name'] = unix_name(p['Name'])
-            print('|-', p['Name'])
-            writepls(p, rootpath, tracks)
-        for f in folders:
-            print('|-', f)
-            ppath = rootpath + unix_name(f) + '/'
-            os.makedirs(ppath)
-            for p in folders[f]:
-                p['Name'] = unix_name(p['Name'])
-                print('|--', p['Name'])
-                writepls(p, ppath, tracks)
+    #     rootpath = 'plsv2/'
+    #     os.makedirs(rootpath, exist_ok=True)
+    #     for p in root:
+    #         p['Name'] = unix_name(p['Name'])
+    #         print('|-', p['Name'])
+    #         writepls(p, rootpath, tracks)
+    #     for f in folders:
+    #         print('|-', f)
+    #         ppath = rootpath + unix_name(f) + '/'
+    #         os.makedirs(ppath)
+    #         for p in folders[f]:
+    #             p['Name'] = unix_name(p['Name'])
+    #             print('|--', p['Name'])
+    #             writepls(p, ppath, tracks)
 
 def unix_name(s):
     s = ''.join(e for e in s if e.isalnum() or e == ' ')
@@ -73,22 +70,23 @@ def unix_name(s):
     return s
 
 
-def writepls(p, ppath, tracks):
-    with open(ppath + p['Name'] + '.m3u', 'w+') as f:
+def writepls(pls_path, pid):
+    pls_tracks = PLAYLISTS[pid].get('Playlist Items', list())
+    with open(pls_path + '.m3u', 'w+') as f:
         f.write('#EXTM3U\n')
-        for t in p.get('Playlist Items', list()):
+        for t in pls_tracks:
             tid = str(t['Track ID'])
             length = 0
             try:
-                length = tracks[tid]['Total Time'] / 1000
+                length = TRACKS[tid]['Total Time'] / 1000
             except Exception as e:
-                print('LENGTH ERROR', tracks[tid]['Location'])
-                artist = tracks[tid].get('Artist', '')
-                name = tracks[tid].get('Name', '')
-                location = tracks[tid]['Location']
-                location = location.replace('localhost/C:/Users/Gebruiker', '/home/user1')
-                f.write('#EXTINF:' + str(length) + ',' + artist + ' - ' + name + '\n')
-                f.write(location + '\n')
+                print('LENGTH ERROR', TRACKS[tid]['Location'])
+            artist = TRACKS[tid].get('Artist', '')
+            name = TRACKS[tid].get('Name', '')
+            location = TRACKS[tid]['Location']
+            location = location.replace('file://localhost/home/user1/Music/iTunes/iTunes%20Media/Music', '/data/music/library/artists')
+            f.write('#EXTINF:' + str(length) + ',' + artist + ' - ' + name + '\n')
+            f.write(urllib.parse.unquote(location) + '\n')
 
 if __name__ == '__main__':
     main()
